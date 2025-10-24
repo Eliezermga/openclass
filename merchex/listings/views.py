3c06010ef5b196df19045840a8c26ffe4751a96f
ELIEZER MUNUNGA <127545012+Eliezermga@users.noreply.github.com>
2025-10-24 20:56:10 +0200

    gabarit

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,14 +1,27 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Band
+from listings.models import Listing
 
 def hello(request):
-    return HttpResponse('<h1>Hello Django!</h1>')
+    bands = Band.objects.all()
+    return render(request, 'listings/hello.html',
+        {'bands': bands})
 
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
 
 def listings(request):
-    return HttpResponse('<h1>Liste des produits</h1> <p>Voici la liste de nos produits disponibles à la vente.</p>')
+    listings = Listing.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django !</h1>
+        <p>Mes titres préférés sont :<p>
+        <ul>
+            <li>{listings[0].title}</li>
+            <li>{listings[1].title}</li>
+            <li>{listings[2].title}</li>
+        </ul>
+""")
 
 def contact(request):
-    return HttpResponse('<h1>Contactez-nous</h1> <p>Vous pouvez nous contacter à...</p>')
+    return render(request, 'listings/contact-us.html')
