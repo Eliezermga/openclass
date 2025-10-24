9e4923e5d5571b5fffcac1c35595678f14ad6eaa
ELIEZER MUNUNGA <127545012+Eliezermga@users.noreply.github.com>
2025-10-24 18:04:07 +0200

    update

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def hello(request):
+    return HttpResponse('<h1>Hello Django!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+
+def listings(request):
+    return HttpResponse('<h1>Liste des produits</h1> <p>Voici la liste de nos produits disponibles à la vente.</p>')
+
+def contact(request):
+    return HttpResponse('<h1>Contactez-nous</h1> <p>Vous pouvez nous contacter à...</p>')
