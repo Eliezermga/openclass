d5f407e0e17c71ca28f404db989e7a5644251f55
ELIEZER MUNUNGA <127545012+Eliezermga@users.noreply.github.com>
2025-10-25 19:34:02 +0200

    Crud

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -3,25 +3,34 @@ from django.shortcuts import render
 from listings.models import Band
 from listings.models import Listing
 
-def hello(request):
+def band_list(request):
     bands = Band.objects.all()
-    return render(request, 'listings/hello.html',
-        {'bands': bands})
+    return render(request,
+            'listings/band_list.html',
+            {'bands': bands})
+
+def band_detail(request, id):
+    band = Band.objects.get(id=id)
+    return render(request,
+            'listings/band_detail.html',
+            {'band': band})
+
+def listing_list(request):
+    listings = Listing.objects.all()
+    return render(request,
+            'listings/listing_list.html',
+            {'listings': listings})
+
+def listing_detail(request, id):
+    listing = Listing.objects.get(id=id)
+    return render(request,
+            'listings/listing_detail.html',
+            {'listing': listing})
 
 def about(request):
     return render(request, 'listings/about.html')
 
-# def listings(request):
-#     listings = Listing.objects.all()
-#     return HttpResponse(f"""
-#         <h1>Hello Django !</h1>
-#         <p>Mes titres préférés sont :<p>
-#         <ul>
-#             <li>{listings[0].title}</li>
-#             <li>{listings[1].title}</li>
-#             <li>{listings[2].title}</li>
-#         </ul>
-# """)
+
 
 def contact(request):
     return render(request, 'listings/contact.html')
