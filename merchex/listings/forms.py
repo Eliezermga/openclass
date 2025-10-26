a17482c9fb5375fd508358a5c446257fb9c60883
ELIEZER MUNUNGA <127545012+Eliezermga@users.noreply.github.com>
2025-10-26 14:47:33 +0100

    forms

--- merchex/listings/forms.py
+++ merchex/listings/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from listings.models import Band, Listing
 
 class ContactUsForm(forms.Form):
     name = forms.CharField(required=False, label="Votre nom")
@@ -8,3 +9,13 @@ class ContactUsForm(forms.Form):
         widget=forms.Textarea(attrs={'rows': 4, 'cols': 40}),
         label="Votre message"
     )
+
+class BandForm(forms.ModelForm):
+   class Meta:
+        model = Band
+        exclude = ('active', 'official_homepage')
+
+class ListingForm(forms.ModelForm):
+   class Meta:
+        model = Listing
+        exclude = ('sold',)
