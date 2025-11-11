83486a5f3488b0592b291220a658152c2913e165
ELIEZER MUNUNGA <127545012+Eliezermga@users.noreply.github.com>
2025-11-11 13:46:34 +0200

    Refactor styles and add Banner styling
    Moved CSS files to a new styles directory and updated imports accordingly. Added Banner.css for custom banner styling and wrapped the Banner component in a styled div.

--- la-maison-jungle/src/components/Banner.js
+++ la-maison-jungle/src/components/Banner.js
@@ -1,5 +1,8 @@
 function Banner() {
-    return <h1>La maison jungle</h1>
+    return (
+    <div className='lmj-banner'>
+                <h1>La maison jungle</h1>
+            </div>)
 }
 
 export default Banner
