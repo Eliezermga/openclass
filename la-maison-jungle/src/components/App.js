e90d22c317618d010d3d627719b3e1a79d8ec28c
ELIEZER MUNUNGA <127545012+Eliezermga@users.noreply.github.com>
2025-11-14 12:45:34 +0200

    Add Essai component and integrate into App
    Introduces a new Essai component with a test message and adds it to the App component's render tree for development and layout testing.

--- la-maison-jungle/src/components/App.js
+++ la-maison-jungle/src/components/App.js
@@ -1,11 +1,14 @@
 import Banner from './Banner'
 import Cart from './Cart'
+import Essai from './Essai'
+
 
 function App() {
 	return (
 		<div>
 			<Banner/>
 			<Cart/>
+			<Essai/>
 		</div>
 	)
 }
---
+++ la-maison-jungle/src/components/Essai.js
@@ -0,0 +1,14 @@
+
+function Essai(){
+    const title = 'bonjour motherfuck'
+    
+    return (
+        <div>
+            <p>
+                ceci est un test depuis; histoir de flex  un peu {title}
+            </p>
+        </div>
+    )
+
+}
+export default Essai
