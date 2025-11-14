b7ac293cea367bc7b296a9fda573e8d34c3dbb95
ELIEZER MUNUNGA <127545012+Eliezermga@users.noreply.github.com>
2025-11-14 12:47:13 +0200

    Remove Essai component and its usage
    Deleted the Essai.js file and removed its import and usage from App.js. This cleans up unused test code from the project.

--- la-maison-jungle/src/components/App.js
+++ la-maison-jungle/src/components/App.js
@@ -1,14 +1,11 @@
 import Banner from './Banner'
 import Cart from './Cart'
-import Essai from './Essai'
-
 
 function App() {
 	return (
 		<div>
 			<Banner/>
 			<Cart/>
-			<Essai/>
 		</div>
 	)
 }
