72e96f15fd888a30e0f6bee542659818f95636da
ELIEZER MUNUNGA <127545012+Eliezermga@users.noreply.github.com>
2025-11-17 17:40:40 +0200

    Add CareScale component and integrate into App
    Introduces the CareScale component to visually represent a care scale using icons. CareScale is imported and rendered in App.js, and ShoppingList.js is updated with minor formatting for future integration.

--- la-maison-jungle/src/components/App.js
+++ la-maison-jungle/src/components/App.js
@@ -1,4 +1,5 @@
 import Banner from './Banner'
+import CareScale from './CareScale'
 import Cart from './Cart'
 import ShoppingList from './ShoppingList'
 
@@ -8,6 +9,7 @@ function App() {
 			<Banner/>
 			<Cart/>
 			<ShoppingList/>
+			<CareScale/>
 		</div>
 	)
 }
